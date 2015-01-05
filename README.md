06ea52b76b06f69ac132aeb12d8c6f1bdd1bb09a
Aleksey Kozachenko <kozachenko.aleksey@gmail.com>
2015-01-05 21:08:25 +0200

    INIT

---
+++ README.md
@@ -0,0 +1,8 @@
+# Ajax pages
+Navigate without page reloading
+
+## Installation
+1. Add #ajax-content wrapper to each page
+2. Add a.ajax-link
+3. Add main.css and ajax-pages.js
+4. Profit
---
+++ ajax-pages.js
@@ -0,0 +1,45 @@
+$(function() {
+  var $main = $("#ajax-content");
+  var $title = document.title;
+  var $body = $("body");
+
+  var loadPage = function(href) {
+    $body.addClass('ajax-loading');
+    setTimeout(function() {
+      $main.load(href + " #ajax-content > *", function(response, status, xhr) {
+        if (status === 'success') {
+          $title = response.match(/<title>(.*?)<\/title>/)[1]
+        } else {
+          loadPage('/404.html');
+        }
+        $body.removeClass('ajax-loading');
+        document.title = $title;
+        $('a.ajax-link').each(function() {
+          $(this).removeClass('active');
+          if ($(this).attr('href') === href) {
+            $(this).addClass('active');
+          }
+        });
+      })
+    }, 300);
+  }
+
+  $(document).on("click", "a.ajax-link", function() {
+    var href = $(this).attr("href");
+    console.log(location);
+    if (href.indexOf('://') === -1 && href.indexOf('#') === -1 && href !== location.pathname) {
+      history.pushState({}, '', href);
+      loadPage(href);
+    }
+    return false;
+  });
+
+  $(window).on("popstate", function(e) {
+    // if (e.originalEvent.state !== null) {
+    //   loadPage(location.href);
+    // }
+    console.log(e);
+    loadPage(location.href);
+  });
+
+});
